18664c0c8c5636b1222d9bfea0d50103ad03865d
mborges <marco.borges@pdmfc.com>
2020-08-12 20:11:21 +0100

    Update with version 1.0.0

--- service01/node_modules/lib/index.js
+++ service01/node_modules/lib/index.js
@@ -1,11 +1,11 @@
 var resp_test = 'Valor em comum'
 
 const resp_service_01 = () => {
-  return 'Resp service 01 - Hello word!'
+  return 'Resp service 01 - Hello word! With Package NodeJS'
 }
 
 const resp_service_02 = () => {
-  return 'Resp service 02 - Hello word!'
+  return 'Resp service 02 - Hello word! With Package NodeJS'
 }
 
 module.exports = {
